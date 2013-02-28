3766af476a13e6934c941f3a70c93b7e07776679
ondmagi <andreas.skarmutsoslindh@gmail.com>
2013-02-28 14:15:24 +0100

    Check for Solaris10/11 link-aggregation via dladm

---
+++ check_solaris_linkaggregation.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+RC=0
+
+if [ -f /opt/op5/plugins/utils.sh ] ; then
+    . /opt/op5/plugins/utils.sh
+fi
+
+# Get list of aggregates
+get_aggrs () {
+	AGGRS=`dladm show-aggr -p|grep ^aggr|awk '{print $2}'|awk -F'=' '{print $2}'`
+	#echo $AGGRS
+}
+
+# Takes one argument: the aggregate key id (integer)
+get_aggr_devices () {
+	AGGRKEY=$1
+	DEVS=`dladm show-aggr -p $AGGRKEY|grep ^dev|awk '{print $3}'|awk -F'=' '{print $2}'`
+	#echo $DEVS
+}
+
+# Get status of a device from dladm, return "up" or "down"
+get_dev_linkstatus () {
+	DEV=$1
+	DEVSTATUS=`dladm show-dev -p|grep $DEV|awk '{print $2}'|awk -F'=' '{print $2}'`
+	#echo $DEVSTATUS
+}
+
+STATUSLINE=""
+get_aggrs
+for aggr in $AGGRS; do
+	get_aggr_devices $aggr
+#	echo $DEVS
+	for dev in $DEVS; do
+		get_dev_linkstatus $dev
+        DEVSTATUS=`echo $DEVSTATUS|tr '[a-z]' '[A-Z]'`
+		STATUSLINE="${STATUSLINE} ${dev}:${DEVSTATUS}"
+        if [ "$DEVSTATUS" != "UP" ]; then
+            RC=$STATE_WARNING
+            BROKENDEVS="${BROKENDEVS} ${dev}"
+        fi
+	done
+done
+
+if [ "$RC" == "$STATE_WARNING" ]; then
+    echo "WARNING: $STATUSLINE - check devices: $BROKENDEVS"
+else
+    echo "OK: $STATUSLINE - all devices are online"
+fi
+exit $RC
